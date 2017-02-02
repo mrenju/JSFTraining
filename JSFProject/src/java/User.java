cf71d5f9687a51fbc7da4884516c0a3518caf15f
renm <renm>
2017-02-02 18:42:47 +0530

    Added a managedbean class User.java. Modified index.xhtml to get a name and Second.xhtml to display it

---
+++ JSFProject/src/java/User.java
@@ -0,0 +1,22 @@
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author renm
+ */
+@ManagedBean
+@SessionScoped
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
